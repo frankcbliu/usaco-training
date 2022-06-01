0c3257e7b43420274589abea5c78542f634a4f9a
frankcbliu <641259875@qq.com>
2022-06-01 19:13:49 +0800

    feat: 1.2.6

--- chapter1/section1_2/friday_the_thirteenth.cpp
+++ chapter1/section1_2/friday_the_thirteenth.cpp
@@ -23,8 +23,8 @@ bool isLeapYear(int year) {
 int main() {
   ofstream fout("friday.out");
   ifstream fin("friday.in");
-  int n = 20;
-//  fin >> n;
+  int n;
+  fin >> n;
   // 1900.1.1, Monday-> (1900+n-1).12.31
   int months[12] = { // 1-12月的天数，2 月的可能需要根据闰年进行处理 +1
           31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
@@ -34,24 +34,25 @@ int main() {
   int days[7] = {0};
 
   // 解法1: 模拟
-  int day = 1;
+  int day = 0 + 12; // 1900.1.1 是 周一
   for (int year = 1900; year < 1900 + n; ++year) {
     bool is_leap = isLeapYear(year);
     for (int i = 0; i < 12; ++i) { // 月份
       // 计算这个月的13号是什么日子
-      day = (day + 12) % 7;
+      day = day % 7;
+//      cout << "年: " << year << " " << i + 1 << "月: 周" << day + 1 << endl;
       days[day]++;
       // 计算这个月有多少天，加上去
       day += months[i];
-      if (i == 1 && is_leap)
+      if (i == 1 && is_leap) // 二月
         day++;
     }
   }
 
   // 输出 13号是周六、周日、一、... 的次数
-  for (int i = 2; i < 9; ++i) {
-    cout << days[i % 7] << " ";
+  for (int i = 5; i < 11; ++i) {
+    fout << days[i % 7] << " ";
   }
-//  fout << a + b << endl;
+  fout << days[4] << '\n';
   return 0;
 }
