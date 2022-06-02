e46d96ec10c3f87cca5a2dcaf683c3248a61453f
frankcbliu <641259875@qq.com>
2022-06-02 22:58:26 +0800

    feat: 1.2.7

--- chapter1/section1_2/broken_necklace.cpp
+++ chapter1/section1_2/broken_necklace.cpp
@@ -9,8 +9,36 @@ LANG: C++
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
+
+int count_pre(string str) {
+  if (str.length() <= 1) {
+    return str.length();
+  }
+  char pre = str.at(0), pw = 'w';
+  int cnt = 0;
+  for (char ch: str) {
+    if (ch != pre && ch != 'w' && pre != 'w') {// 先考虑退出循环的情况
+      break;
+    }
+    if (ch == pre) { // 先考虑当前值能继续的情况
+      cnt++;
+    } else if (ch == 'w') {
+      cnt++;
+      if (pre != 'w')
+        pw = pre;
+    } else if (pre == 'w' && (ch == pw || pw == 'w')) {
+      cnt++;
+      pw = ch;
+    } else {
+      break;
+    }
+    pre = ch;
+  }
+  return cnt;
+}
 
 int main() {
   ofstream fout("beads.out");
@@ -18,18 +46,22 @@ int main() {
   int n;
   string circle;
   fin >> n >> circle;
-//  circle = circle + circle;
-  cout << circle << endl;
-  circle = "11234511234561111211112312345";
-  std::reverse(circle.begin(), circle.end());
-  cout << circle << endl;
-
-  // wwwbbrwrbrbrrbrbrwrwwrbwrwrrb
-  // 12345123111121111234561223451
-  // brrwrwbrwwrwrbrbrrbrbrwrbbwww
-  // 11234511234561111211112312345
-  // 54321321111211116543211543211
-
-//  fout << a + b << endl;
+//  for debug
+//  circle = "rrwwwwbb";
+//  n = circle.length();
+  circle = circle + circle;
+  int max = 0;
+  for (int i = 1; i < n + 1; ++i) {
+    string left = circle.substr(0, i);
+    std::reverse(left.begin(), left.end());
+    int left_cnt = count_pre(left);
+    string right = circle.substr(i, n - left_cnt);
+// for debug
+//    cout << left << " " << right << endl;
+//    cout << left_cnt << " " << count_pre(right) << endl;
+    max = std::max(max, left_cnt + count_pre(right));
+  }
+  fout << max << endl;
+//  cout << max << endl; // for debug
   return 0;
 }
