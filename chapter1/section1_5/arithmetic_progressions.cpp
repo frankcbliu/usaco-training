6bd085f849ca98d8479163d1b2ebd36c3e1f6541
frankcbliu <q641259875@outlook.com>
2022-06-18 14:53:30 +0800

    feat 1.5.2

--- chapter1/section1_5/arithmetic_progressions.cpp
+++ chapter1/section1_5/arithmetic_progressions.cpp
@@ -10,6 +10,7 @@ LANG: C++
 #include <fstream>
 #include <algorithm>
 #include <vector>
+#include <set>
 #include <cassert>
 
 using namespace std;
@@ -17,6 +18,7 @@ using namespace std;
 int N, M; // n: 3~25  m: 1~250
 vector<pair<int, int>> ans;
 bool m[2 * 250 * 250 + 1];
+vector<int> a0_list;
 
 bool check(int a0, int d) {
   for (int n = 0; n < N; ++n) {
@@ -27,7 +29,9 @@ bool check(int a0, int d) {
 
 void force_search() {
   int m2 = 2 * M * M;
-  for (int a0 = 0; a0 < m2; ++a0) {
+//  for (int a0 = 0; a0 < m2 - N + 1; ++a0) { //
+  for (int i = 0; i < a0_list.size() - N + 1; ++i) { // 用 a0_list 可以优化速度，但是空间占用会更高一些
+    int a0 = a0_list[i];
     for (int d = 1; d <= (m2 - a0) / (N - 1); ++d) { // 关键优化点 d <= (2*M^2 - a0) / (n-1)
       if (check(a0, d)) {
         ans.emplace_back(d, a0);
@@ -40,9 +44,15 @@ int main() {
   ofstream fout("ariprog.out");
   ifstream fin("ariprog.in");
   fin >> N >> M;
+  set<int> tmp_set;
   for (int i = 0; i <= M; ++i)
-    for (int j = 0; j <= M; ++j)
-      m[i * i + j * j] = true;
+    for (int j = 0; j <= M; ++j) {
+      int v = i * i + j * j;
+      m[v] = true;
+      tmp_set.insert(v);
+    }
+  for (auto a0: tmp_set)
+    a0_list.push_back(a0);
 
   // 暴力搜索
   force_search();
